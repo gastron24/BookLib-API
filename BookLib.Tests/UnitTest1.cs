bc33956c94ac77523f0ddf14cb169c13763a2ec5
Alexander <ppepa7916@gmail.com>
2025-05-27 18:12:07 +0300

    ковыряюсь в тестах!

--- BookLib.Tests/UnitTest1.cs
+++ BookLib.Tests/UnitTest1.cs
@@ -1,10 +1,13 @@
-﻿namespace BookLib.Tests;
+﻿using BookLib.Tests;
+namespace BookLib.Tests;
 
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public async Task CreateBookAsync_ShouldThrowException_WhenYearIsInFuture()
     {
-
+        
+       
     }
 }
+
